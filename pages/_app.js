b9fff75ac5f7cebab0928b4d11ad8dfbce687ad2
Alex Alimov <aalimov@asu.edu>
2023-05-28 16:38:45 -0700

    Implement base tailwind configuration, including fonts and colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Inconsolata, Inter, Lora } from 'next/font/google';
+import Head from 'next/head';
+
+const InterFont = Inter({
+  variable: '--font-inter',
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const LoraFont = Lora({
+  variable: '--font-lora',
+  weight: ['400', '500'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const InconsolataFont = Inconsolata({
+  variable: '--font-inconsolata',
+  weight: ['400', '500'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={`${InterFont.variable} ${LoraFont.variable} ${InconsolataFont.variable} font-sans`}>
+      <Head>
+        <title>Dictionary App</title>
+        <meta name='description' content='Search for the definition of a word' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,108 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': theme('colors.gray[700]'),
+            '--tw-prose-headings': theme('colors.gray[700]'),
+            '--tw-prose-links': theme('colors.purple[500]'),
+            '--tw-prose-links-hover': theme('colors.purple[600]'),
+
+            '--tw-prose-invert-body': theme('colors.purple[200]'),
+            '--tw-prose-invert-headings': theme('colors.white'),
+            '--tw-prose-invert-links': theme('colors.white'),
+            '--tw-prose-invert-links-hover': theme('colors.white'),
+
+            // Base
+            color: 'var(--tw-prose-body)',
+            lineHeight: theme('lineHeight.7'),
+            '> *': {
+              marginTop: theme('spacing.10'),
+              marginBottom: theme('spacing.10'),
+            },
+            p: {
+              marginTop: theme('spacing.7'),
+              marginBottom: theme('spacing.7'),
+            },
+
+            // Headings
+            'h1, h2, h3': {
+              color: 'var(--tw-prose-headings)',
+              fontWeight: theme('fontWeight.bold'),
+            },
+            h1: {
+              fontSize: '4rem',
+              lineHeight: theme('lineHeight.7'),
+              marginTop: theme('spacing.20'),
+              marginBottom: theme('spacing.4'),
+            },
+            h2: {
+              fontSize: theme('fontSize.2xl')[0],
+              lineHeight: theme('lineHeight.7'),
+              marginTop: theme('spacing.20'),
+              marginBottom: theme('spacing.4'),
+            },
+            h3: {
+              fontSize: theme('fontSize.lg')[0],
+              lineHeight: theme('lineHeight.7'),
+              marginTop: theme('spacing.16'),
+              marginBottom: theme('spacing.4'),
+            },
+            ':is(h2, h3) + *': {
+              marginTop: 0,
+            },
+            // Inline elements
+            a: {
+              color: 'var(--tw-prose-links)',
+              fontWeight: theme('fontWeight.semibold'),
+              textDecoration: 'underline',
+              textDecorationColor: 'var(--tw-prose-underline)',
+              transitionProperty: 'color, text-decoration-color',
+              transitionDuration: theme('transitionDuration.150'),
+              transitionTimingFunction: theme('transitionTimingFunction.in-out'),
+            },
+            'a:hover': {
+              color: 'var(--tw-prose-links-hover)',
+              textDecorationColor: 'var(--tw-prose-underline-hover)',
+            },
+          },
+        },
+      }),
+      fontSize: {
+        xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+        sm: ['0.875rem', { lineHeight: '1.5rem' }],
+        base: ['1rem', { lineHeight: '1.75rem' }],
+        lg: ['1.125rem', { lineHeight: '1.75rem' }],
+        xl: ['1.25rem', { lineHeight: '2rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '4xl': ['2rem', { lineHeight: '2.5rem' }],
+        '5xl': ['3rem', { lineHeight: '3.5rem' }],
+        '6xl': ['3.75rem', { lineHeight: '1' }],
+        '7xl': ['4.5rem', { lineHeight: '1' }],
+        '8xl': ['6rem', { lineHeight: '1' }],
+        '9xl': ['8rem', { lineHeight: '1' }],
+      },
+      colors: {
+        gray: {
+          900: '#050505',
+          800: '#1F1F1F',
+          700: '#2D2D2D',
+          600: '#3A3A3A',
+          500: '#757575',
+          400: '#E9E9E9',
+          300: '#F4F4F4',
+        },
+        purple: '#A445ED',
+        red: '#FF5252',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+        serif: ['var(--font-lora)', 'serif'],
+        mono: ['var(--font-inconsolata)', 'monospace'],
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
